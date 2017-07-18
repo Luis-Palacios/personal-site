2d3e04cd27eed331d46841956675043ba35d2f0d
Luis Palacios <lrpalaciosdev@gmail.com>
2017-07-18 16:11:52 -0600

    add webpack config for fonts

--- PersonalLuis.Site/webpack.config.js
+++ PersonalLuis.Site/webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
             }
-        ]
+        ],
+
     }
 };
--- PersonalLuis.Site/wwwroot/index.js
+++ PersonalLuis.Site/wwwroot/index.js
@@ -1,2 +1,3 @@
 ﻿import 'normalize.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
 console.log("The bundle was loaded");
