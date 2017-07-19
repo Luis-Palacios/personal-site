0021468c4a467542421cb40939403e296b206f10
LRPalacios <lrpalaciosdev@gmail.com>
2017-07-19 15:19:53 -0600

    add initial model

---
+++ PersonalLuis.Site/Models/Personal/GeneralInfo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PersonalLuis.Site.Models.Personal
+{
+    public class GeneralInfo
+    {
+        public string History {get; set;}
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
