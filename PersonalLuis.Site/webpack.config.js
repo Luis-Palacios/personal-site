e56a64f6331812b8199b680bcf0eae16509786d6
Luis Palacios <lrpalaciosdev@gmail.com>
2019-05-16 19:38:38 -0600

    add next gen format

--- PersonalLuis.Site/webpack.config.js
+++ PersonalLuis.Site/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|webp)$/,
         use: [
           'file-loader',
         ],
