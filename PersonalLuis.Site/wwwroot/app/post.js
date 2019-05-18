a4549ed8fbe114b96ae4254ef0ffaa20a0ce16dc
Luis Palacios <lrpalaciosdev@gmail.com>
2019-05-18 11:29:21 -0600

    add deferred image loading for post

--- PersonalLuis.Site/wwwroot/app/post.js
+++ PersonalLuis.Site/wwwroot/app/post.js
@@ -26,3 +26,23 @@ document.getElementById('linkedin-share').addEventListener('click', () => {
   const url = window.location.href;
   window.open(`http://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(url)}`, '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
 });
+
+const targets = document.querySelectorAll('.single-post-content img');
+
+const lazyLoad = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-lazy');
+
+        img.setAttribute('src', src);
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+targets.forEach(lazyLoad);
