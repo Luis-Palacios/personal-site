e62ee0c3a4589a63005b9b54b8b9b7eb000248b2
Luis Palacios <lrpalaciosdev@gmail.com>
2019-05-26 17:42:27 -0600

    add img-lazy tag helper

---
+++ PersonalLuis.Site/TagHelpers/LazyImgTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+
+namespace PersonalLuis.Site.TagHelpers
+{
+    [HtmlTargetElement("lazy-img", TagStructure = TagStructure.WithoutEndTag)]
+    public class LazyImgTagHelper: TagHelper
+    {
+        public string Src { get; set; }
+        public string Class { get; set; }
+        public string Alt { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+            output.TagName = "img";
+            output.Attributes.SetAttribute("data-src", Src);
+            output.Attributes.SetAttribute("src", "/images/blur.png");
+            output.Attributes.SetAttribute("class", "lazy");
+            output.AddClass(Class, HtmlEncoder.Default);
+            output.Attributes.SetAttribute("alt", Alt);
+        }
+    }
+}
